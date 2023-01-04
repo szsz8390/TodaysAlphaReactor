e406f207949b5cd91989605d6009d2fb88d32339
szsz8390 <suzu.sn.yuuki@gmail.com>
2023-01-04 19:36:45 +0900

    多重起動防止

--- App.xaml.cs
+++ App.xaml.cs
@@ -13,5 +13,52 @@ namespace TodaysAlphaReactor
     /// </summary>
     public partial class App : Application
     {
+        /// <summary>二重起動防止用セマフォの名前</summary>
+        private const string SemaphoreName = "TodaysAlphaReactor";
+
+        /// <summary>二重起動防止用セマフォ</summary>
+        private System.Threading.Semaphore _semaphore;
+
+        /// <summary>
+        /// 開始時処理
+        /// セマフォを作成し、新規作成の場合のみ起動処理を行う。
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            bool createdNew;
+            _semaphore = new System.Threading.Semaphore(1, 1, SemaphoreName, out createdNew);
+            // 新規作成なら通常起動、既にセマフォが作成されていた場合は二重起動なので終了させる
+            if (createdNew)
+            {
+                base.OnStartup(e);
+            }
+            else
+            {
+                this.Shutdown();
+            }
+        }
+
+        /// <summary>
+        /// 終了時処理
+        /// セマフォを解放する。
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+#if DEBUG
+            try
+            {
+                _semaphore?.Release();
+            }
+            catch (Exception ex) when (ex is System.Threading.SemaphoreFullException)
+            {
+                //
+            }
+#else
+            _semaphore?.Release();
+#endif
+        }
     }
 }
